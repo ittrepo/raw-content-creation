ce5e539c2a60e8c8682f5e48ab5ed81d04077aeb
roknuzzaman <rokon.raz@gmail.com>
2025-03-10 15:22:56 +0600

    Add provider mapping for giata.

--- constant_file/for_database.py
+++ constant_file/for_database.py
@@ -24,3 +24,35 @@ PROVIDER_MAPPING_FOR_DB = {
     "innstanttravel": ["innstanttravel", "innstanttravel_a", "innstanttravel_b", "innstanttravel_c", "innstanttravel_d", "innstanttravel_e"],
     "roomerang": ["roomerang", "roomerang_a", "roomerang_b", "roomerang_c", "roomerang_d", "roomerang_e"]
 }
+
+
+
+
+
+
+PROVIDER_MAPPING_FOR_SUPPLIER = {
+    "hotelbeds": ["hotelbeds", "hotelbeds_a", "hotelbeds_b", "hotelbeds_c", "hotelbeds_d", "hotelbeds_e"],
+    "ean": ["ean", "ean_a", "ean_b", "ean_c", "ean_d", "ean_e"],
+    "agoda": ["agoda", "agoda_a", "agoda_b", "agoda_c", "agoda_d", "agoda_e"],
+    "mgholiday": ["mgholiday", "mgholiday_a", "mgholiday_b", "mgholiday_c", "mgholiday_d", "mgholiday_e"],
+    "restel": ["restel", "restel_a", "restel_b", "restel_c", "restel_d", "restel_e"],
+    "stuba": ["stuba", "stuba_a", "stuba_b", "stuba_c", "stuba_d", "stuba_e"],
+    "hyperguestdirect": ["hyperguestdirect", "hyperguestdirect_a", "hyperguestdirect_b", "hyperguestdirect_c", "hyperguestdirect_d", "hyperguestdirect_e"],
+    "TravelBoutiqueOnline": ["tbohotel", "tbohotel_a", "tbohotel_b", "tbohotel_c", "tbohotel_d", "tbohotel_e"],
+    "goglobal": ["goglobal", "goglobal_a", "goglobal_b", "goglobal_c", "goglobal_d", "goglobal_e"],
+    "ratehawk2": ["ratehawkhotel", "ratehawkhotel_a", "ratehawkhotel_b", "ratehawkhotel_c", "ratehawkhotel_d", "ratehawkhotel_e"],
+    "adivahahotel": ["adivahahotel", "adivahahotel_a", "adivahahotel_b", "adivahahotel_c", "adivahahotel_d", "adivahahotel_e"],
+    "grnconnect": ["grnconnect", "grnconnect_a", "grnconnect_b", "grnconnect_c", "grnconnect_d", "grnconnect_e"],
+    "juniper": ["juniperhotel", "juniperhotel_a", "juniperhotel_b", "juniperhotel_c", "juniperhotel_d", "juniperhotel_e"],
+    "mikihotel": ["mikihotel", "mikihotel_a", "mikihotel_b", "mikihotel_c", "mikihotel_d", "mikihotel_e"],
+    "paximum": ["paximumhotel", "paximumhotel_a", "paximumhotel_b", "paximumhotel_c", "paximumhotel_d", "paximumhotel_e"],
+    "adonishotel": ["adonishotel", "adonishotel_a", "adonishotel_b", "adonishotel_c", "adonishotel_d", "adonishotel_e"],
+    "w2mhotel": ["w2mhotel", "w2mhotel_a", "w2mhotel_b", "w2mhotel_c", "w2mhotel_d", "w2mhotel_e"],
+    "oryxhotel": ["oryxhotel", "oryxhotel_a", "oryxhotel_b", "oryxhotel_c", "oryxhotel_d", "oryxhotel_e"],
+    "dotw": ["dotw", "dotw_a", "dotw_b", "dotw_c", "dotw_d", "dotw_e"],
+    "hotelston": ["hotelston", "hotelston_a", "hotelston_b", "hotelston_c", "hotelston_d", "hotelston_e"],
+    "letsfly": ["letsflyhotel", "letsflyhotel_a", "letsflyhotel_b", "letsflyhotel_c", "letsflyhotel_d", "letsflyhotel_e"],
+    "iol_iwtx": ["illusionshotel", "illusionshotel_a", "illusionshotel_b", "illusionshotel_c", "illusionshotel_d", "illusionshotel_e"],
+    "innstanttravel": ["innstanttravel", "innstanttravel_a", "innstanttravel_b", "innstanttravel_c", "innstanttravel_d", "innstanttravel_e"],
+    "roomerang": ["roomerang", "roomerang_a", "roomerang_b", "roomerang_c", "roomerang_d", "roomerang_e"]
+}
