4acc84a0b051517f4252fe0a73270420a51c096a
roknuzzaman <rokon.raz@gmail.com>
2025-04-05 21:12:38 +0600

    JEAC

--- supplier/restel/main.py
+++ supplier/restel/main.py
@@ -12,9 +12,9 @@ load_dotenv()
 # Constants
 HOTEL_ID_LIST = "D:/Rokon/ofc_git/row_content_create/hotel_id_count_function/restel/restel_hotel_id_list.txt"
 TRACKING_FILE = "D:/Rokon/ofc_git/row_content_create/hotel_id_count_function/restel/restel_tracking_file.txt"
-SUCCESS_FILE = "D:/Rokon/ofc_git/row_content_create/hotel_id_count_function/restel/successful_done_hotel_id_list.txt"
-NOT_FOUND_FILE = "D:/Rokon/ofc_git/row_content_create/hotel_id_count_function/restel/restel_hotel_not_found.txt"
-BASE_PATH = "D:/content_for_hotel_json/cdn_row_collection/restel"
+SUCCESS_FILE =
+NOT_FOUND_FILE =
+BASE_PATH =
 
 
 REQUEST_DELAY = 1
